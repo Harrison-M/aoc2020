9a5d8a07c88d99be424001099707b1593fbac134
Hal Massey <harrison.massey@gmail.com>
2020-12-09 21:40:06 -0600

    feat: Day 6, Part 2
    Found out you can collect() into HashSets

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -3,16 +3,33 @@ use std::env;
 use std::fs;
 
 /// Find the number of unique responses per group and sum them
-fn part1(responses: String) -> usize {
+fn part1(responses: &String) -> usize {
     responses.split("\n\n")
         .fold(0, |acc, group| {
-            let mut answer_set: HashSet<char> = HashSet::new();
-            for c in group.chars() {
-                if c.is_alphabetic() {
-                    answer_set.insert(c);
-                }
-            }
+            let answer_set: HashSet<_> = group.chars()
+                .filter(|c| c.is_alphabetic()).collect();
             acc + answer_set.len()
+        })
+}
+
+/// Find the number of responses every member of a group marked
+fn part2(responses: &String) -> usize {
+    responses.split("\n\n")
+        .fold(0, |acc, group| {
+            group.lines()
+                .fold(None, |group_acc: Option<HashSet<char>>, survey| {
+                    let answer_set: HashSet<char> = survey.chars().collect();
+                    match group_acc {
+                        None => Some(answer_set),
+                        Some(acc_set) =>
+                            Some(
+                                acc_set
+                                    .intersection(&answer_set)
+                                    .cloned()
+                                    .collect()
+                            )
+                    }
+                }).map_or(0, |s| s.len()) + acc
         })
 }
 
@@ -21,15 +38,23 @@ fn main() {
     let filename = &args[1];
 
     let contents = fs::read_to_string(filename).expect("Error opening file");
-    println!("Part 1: {}", part1(contents));
+    println!("Part 1: {}", part1(&contents));
+    println!("Part 2: {}", part2(&contents));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const SAMPLE: &str = include_str!("sample");
+
     #[test]
     fn part1_example() {
-        assert_eq!(part1(include_str!("sample").to_string()), 11);
+        assert_eq!(part1(&SAMPLE.to_string()), 11);
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(&SAMPLE.to_string()), 6);
     }
 }
