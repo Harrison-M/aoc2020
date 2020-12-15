4802433b540ced0e9fbd59f8e0e0b7a4c058333a
Hal Massey <harrison.massey@gmail.com>
2020-12-14 20:29:20 -0600

    feat: Day 10, Part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -28,6 +28,41 @@ fn part1(adapters: &Vec<usize>) -> usize {
             + 1) // Include the device
 }
 
+// TODO: try just multiplying the diffs
+fn adapter_possibilities(adapters: &[usize], memos: &mut HashMap<usize, usize>) -> usize {
+    match adapters.split_first() {
+        Some((&first, rest)) => {
+            if let Some(&memo) = memos.get(&first) {
+                memo
+            } else if rest.len() == 0 {
+                memos.insert(first, 1);
+                1
+            } else {
+                let mut acc = 0;
+                let mut rest_candidate = rest.clone();
+                loop {
+                    match rest_candidate.first() {
+                        Some(next) if next - first <= 3 =>
+                            acc += adapter_possibilities(rest_candidate, memos),
+                        _ => break,
+                    }
+                    rest_candidate = &rest_candidate[1..];
+                }
+                memos.insert(first, acc);
+                acc
+            }
+        }
+        None => panic!("Base case should occur before we're out of adapters"),
+    }
+}
+
+fn part2(adapters: &Vec<usize>) -> usize {
+    let mut all_adapters = adapters.clone();
+    all_adapters.push(0);
+    all_adapters.sort_unstable();
+    adapter_possibilities(&all_adapters[..], &mut HashMap::new())
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
@@ -35,6 +70,7 @@ fn main() {
     let contents = fs::read_to_string(filename).expect("Error opening file");
     let numbers: Vec<usize> = contents.lines().map(|l| l.parse().unwrap()).collect();
     println!("Part 1: {}", part1(&numbers));
+    println!("Part 2: {}", part2(&numbers));
 }
 
 #[cfg(test)]
@@ -50,5 +86,16 @@ mod tests {
 
         assert_eq!(part1(&sample1), 35);
         assert_eq!(part1(&sample2), 220);
+    }
+
+    #[test]
+    fn part2_examples() {
+        let sample1 = vec![16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4];
+        let sample2 = vec![28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49,
+                           45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4,
+                           2, 34, 10, 3];
+
+        assert_eq!(part2(&sample1), 8);
+        assert_eq!(part2(&sample2), 19208);
     }
 }
