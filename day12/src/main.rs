fc55296109df066f5666f4e2b88bcc4bcc206850
Hal Massey <harrison.massey@gmail.com>
2020-12-15 22:50:25 -0600

    feat(day12): Day 12, Part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -82,9 +82,60 @@ impl MovingFerry {
     }
 }
 
+struct WaypointFerry {
+    position: Position,
+    waypoint: Position,
+}
+
+impl WaypointFerry {
+    fn new() -> Self {
+        Self {
+            position: Position(0, 0),
+            waypoint: Position(10, 1),
+        }
+    }
+
+    /// Rotate the ship, assuming the facing is locked to a cardinal direction
+    fn rotate(&mut self, direction: RotationDir, degrees: isize) {
+        let facing_skip = degrees / 90;
+        for _ in 0..facing_skip {
+            let Position(x, y) = self.waypoint;
+            match direction {
+                RotationDir::LEFT => self.waypoint = Position(-1 * y, x),
+                RotationDir::RIGHT => self.waypoint = Position(y, -1 * x),
+            }
+        }
+    }
+
+    /// Update the ferry according to the given instruction
+    fn step(&mut self, instruction: &str) {
+        let (action, value_str) = instruction.split_at(1);
+        let value: isize = value_str.parse().unwrap();
+        match action {
+            "N" => self.waypoint += Position(0, 1) * value,
+            "S" => self.waypoint += Position(0, -1) * value,
+            "E" => self.waypoint += Position(1, 0) * value,
+            "W" => self.waypoint += Position(-1, 0) * value,
+            "L" => self.rotate(RotationDir::LEFT, value),
+            "R" => self.rotate(RotationDir::RIGHT, value),
+            "F" => self.position += self.waypoint * value,
+            a => panic!("Unrecognized action {}", a),
+        }
+    }
+}
+
 /// Find the manhattan distance traveled after a ferry follows the instructions
 fn part1<'a>(instructions: impl Iterator<Item = &'a str>) -> isize {
     let mut ferry = MovingFerry::new();
+    for instruction in instructions {
+        ferry.step(instruction);
+    }
+    ferry.position.0.abs() + ferry.position.1.abs()
+}
+
+/// Find the manhattan distance traveled after a ferry follows the *real* instructions
+fn part2<'a>(instructions: impl Iterator<Item = &'a str>) -> isize {
+    let mut ferry = WaypointFerry::new();
     for instruction in instructions {
         ferry.step(instruction);
     }
@@ -97,6 +148,7 @@ fn main() {
 
     let contents = fs::read_to_string(filename).expect("Error opening file");
     println!("Part 1: {}", part1(contents.lines()));
+    println!("Part 2: {}", part2(contents.lines()));
 }
 
 #[cfg(test)]
@@ -107,5 +159,11 @@ mod test {
     fn part1_example() {
         let instructions = vec!["F10", "N3", "F7", "R90", "F11"];
         assert_eq!(part1(instructions.into_iter()), 25);
+    }
+
+    #[test]
+    fn part2_example() {
+        let instructions = vec!["F10", "N3", "F7", "R90", "F11"];
+        assert_eq!(part2(instructions.into_iter()), 286);
     }
 }
