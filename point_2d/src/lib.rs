58d0235f12db8112acf2f8179a19e878396e9521
Hal Massey <harrison.massey@gmail.com>
2020-12-25 00:56:17 -0600

    feat(point_2d): Implement Add and Sub for &Point2D

--- point_2d/src/lib.rs
+++ point_2d/src/lib.rs
@@ -13,11 +13,27 @@ impl<T: Add<T, Output=T>> Add for Point2D<T> {
     }
 }
 
+impl<T: Add<T, Output=T> + Copy> Add for &Point2D<T> {
+    type Output = Point2D<T>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Point2D(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+
 impl<T: Add<T, Output=T>> Add<(T, T)> for Point2D<T> {
     type Output = Point2D<T>;
 
     fn add(self, rhs: (T, T)) -> Self::Output {
         Self(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+
+impl<T: Add<T, Output=T> + Copy> Add<(T, T)> for &Point2D<T> {
+    type Output = Point2D<T>;
+
+    fn add(self, rhs: (T, T)) -> Self::Output {
+        Point2D(self.0 + rhs.0, self.1 + rhs.1)
     }
 }
 
@@ -41,11 +57,27 @@ impl<T: Sub<T, Output=T>> Sub for Point2D<T> {
     }
 }
 
+impl<T: Sub<T, Output=T> + Copy> Sub for &Point2D<T> {
+    type Output = Point2D<T>;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Point2D(self.0 - rhs.0, self.1 - rhs.1)
+    }
+}
+
 impl<T: Sub<T, Output=T>> Sub<(T, T)> for Point2D<T> {
     type Output = Point2D<T>;
 
     fn sub(self, rhs: (T, T)) -> Self::Output {
         Self(self.0 - rhs.0, self.1 - rhs.1)
+    }
+}
+
+impl<T: Sub<T, Output=T> + Copy> Sub<(T, T)> for &Point2D<T> {
+    type Output = Point2D<T>;
+
+    fn sub(self, rhs: (T, T)) -> Self::Output {
+        Point2D(self.0 - rhs.0, self.1 - rhs.1)
     }
 }
 
@@ -69,6 +101,14 @@ impl<T: Mul<T, Output=T> + Copy> Mul<T> for Point2D<T> {
     }
 }
 
+impl<T: Mul<T, Output=T> + Copy> Mul<T> for &Point2D<T> {
+    type Output = Point2D<T>;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        Point2D(self.0 * rhs, self.1 * rhs)
+    }
+}
+
 impl<T: Mul<T, Output=T> + Copy> MulAssign<T> for Point2D<T> {
     fn mul_assign(&mut self, rhs: T) {
         *self = Self(self.0 * rhs, self.1 * rhs)
@@ -80,6 +120,14 @@ impl<T: Div<T, Output=T> + Copy> Div<T> for Point2D<T> {
 
     fn div(self, rhs: T) -> Self::Output {
         Self(self.0 / rhs, self.1 / rhs)
+    }
+}
+
+impl<T: Div<T, Output=T> + Copy> Div<T> for &Point2D<T> {
+    type Output = Point2D<T>;
+
+    fn div(self, rhs: T) -> Self::Output {
+        Point2D(self.0 / rhs, self.1 / rhs)
     }
 }
 
